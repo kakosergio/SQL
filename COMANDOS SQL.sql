a6463eb7e02e5c2ca830aa1814bfc98d0a490eaa
Senrathy <sergioricardort@hotmail.com>
2023-01-21 20:31:39 -0300

    first commit

---
+++ COMANDOS SQL.sql
@@ -0,0 +1,12 @@
+COMANDOS SQL
+
+SELECT
+-- SQL SERVER, Postgres, Oracle, mySQL
+
+-- Selecionar dados de determinadas colunas
+SELECT coluna1, coluna2
+FROM tabela
+
+-- Selecionar todas as colunas de uma determinada tabela
+SELECT *
+FROM tabela 
